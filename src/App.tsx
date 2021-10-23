75efbff75ae556824631c6d7268f4f62a7c19be6
Chupriarti <wallx13@gmail.com>
2021-10-23 21:52:57 +0300

    added Board

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Board from './components/Board/Board';
 
 function App() {
   return (
     <div className="App">
       <h1>React App</h1>
+      <Board />
     </div>
   );
 }
---
+++ src/components/Board/Board.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Board.css';
+
+const Board = (props: any) => {
+  return (
+    <div className="Board">
+    </div>
+  );
+}
+
+export default Board;
