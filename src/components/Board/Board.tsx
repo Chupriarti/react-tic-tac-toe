4a33eca8b929ff33f5248cb704b946628f258c05
Chupriarti <wallx13@gmail.com>
2021-10-24 21:45:59 +0300

    added player state

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -4,18 +4,20 @@ import './Board.css';
 
 const Board = (props: any) => {
 
+  const [player, setPlayer] = React.useState("X");
   const [values, setValues] = React.useState<any[]>([
     null, null, null,
     null, null, null,
     null, null, null
   ]);
 
-  const onChange = (idx: number) => {
-    setValues(values => [...values.slice(0, idx), "X",  ...values.slice(idx + 1)])
+  const onChange = (idx: number, player: string) => {
+    setValues(values => [...values.slice(0, idx), player,  ...values.slice(idx + 1)]);
+    setPlayer(player => player === "X" ? "0" : "X")
   }
   return (
     <div className="Board">
-        {values.map((item, idx) => <Field key={idx} value={item} onClick={() => onChange(idx)} />)}
+        {values.map((item, idx) => <Field key={idx} value={item} onClick={() => onChange(idx, player)} />)}
     </div>
   );
 }
