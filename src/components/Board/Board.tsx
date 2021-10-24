a1a8746be1cf6095aa8a752abc71164e8946dc9e
Chupriarti <wallx13@gmail.com>
2021-10-24 21:53:19 +0300

    refactoring

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -5,17 +5,14 @@ import './Board.css';
 const Board = (props: any) => {
 
   const [player, setPlayer] = React.useState("X");
-  const [values, setValues] = React.useState<any[]>([
-    null, null, null,
-    null, null, null,
-    null, null, null
-  ]);
+  const [values, setValues] = React.useState<any[]>( new Array(9).fill(null));
 
   const onChange = (idx: number, player: string) => {
     if (values[idx] !== null) return;
     setValues(values => [...values.slice(0, idx), player,  ...values.slice(idx + 1)]);
     setPlayer(player => player === "X" ? "0" : "X")
   }
+  
   return (
     <div className="Board">
         {values.map((item, idx) => <Field key={idx} value={item} onClick={() => onChange(idx, player)} />)}
