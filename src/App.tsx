945c9097a53ffddc138347c632ca5e782c9ebb6b
Chupriarti <wallx13@gmail.com>
2021-10-25 07:56:28 +0300

    added winner calculation

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import Board from './components/Board/Board';
 
 function App() {
 
-  const [player, setPlayer] = React.useState("X");
+  const [player, setPlayer] = React.useState<string>("0");
+  const [winner, setWinner] = React.useState<string>("");
 
   return (
     <div className="App">
       <h1>Current Player: {player}</h1>
-      <Board player={player} setPlayer={setPlayer} />
+      <Board player={player} setPlayer={setPlayer} setWinner={setWinner} />
+      {winner && <h1>Winner: {winner}</h1>}
     </div>
   );
 }
--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -3,18 +3,40 @@ import Field from '../Field/Field';
 import './Board.css';
 
 type BoardProps = {
-  player: string
+  player: string;
 	setPlayer: (player: any) => void;
+  setWinner: (winner: any) => void;
 };
 
 const Board: React.FC<BoardProps> = (props) => {
 
   const [values, setValues] = React.useState<any[]>( new Array(9).fill(null));
 
+  React.useEffect(() => {
+    if (checkIsWinner(values, props.player)){
+      props.setWinner(props.player)
+    }
+    props.setPlayer(props.player === "X" ? "0" : "X")
+  }, [values])
+
+
+  const checkIsWinner = (vals: any[], player: string) => {
+    if (
+      (vals[0] === player && vals[1] === player && vals[2] === player) ||
+      (vals[0] === player && vals[3] === player && vals[6] === player) ||
+      (vals[2] === player && vals[5] === player && vals[8] === player) ||
+      (vals[6] === player && vals[7] === player && vals[8] === player) ||
+      (vals[0] === player && vals[4] === player && vals[8] === player) ||
+      (vals[3] === player && vals[4] === player && vals[5] === player) ||
+      (vals[1] === player && vals[4] === player && vals[7] === player) ||
+      (vals[2] === player && vals[4] === player && vals[6] === player)
+    ) return true
+    return false;
+  }
+
   const onChange = (idx: number, player: string) => {
     if (values[idx] !== null) return;
     setValues(values => [...values.slice(0, idx), player,  ...values.slice(idx + 1)]);
-    props.setPlayer(props.player === "X" ? "0" : "X")
   }
   
   return (
